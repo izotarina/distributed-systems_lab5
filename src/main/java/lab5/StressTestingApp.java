43feb025fe0cdad7eb3bf5809475f1f56e7045c5
izotarina <irunchikt@mail.ru>
2021-12-12 11:41:19 +0300

    Scripted auto-commit on change (2021-12-12 11:41:19) by gitwatch.sh

--- src/main/java/lab5/StressTestingApp.java
+++ src/main/java/lab5/StressTestingApp.java
@@ -27,8 +27,7 @@ public class StressTestingApp {
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8080/\nPress
-                RETURN to stop...");
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
