5650b75aa16c628a95dfca3318da07a271d2fafb
izotarina <irunchikt@mail.ru>
2021-12-12 11:47:53 +0300

    Scripted auto-commit on change (2021-12-12 11:47:53) by gitwatch.sh

--- src/main/java/lab5/StorageActor.java
+++ src/main/java/lab5/StorageActor.java
@@ -3,5 +3,16 @@ package lab5;
 import akka.actor.AbstractActor;
 
 public class StorageActor extends AbstractActor {
-    
+    private Map<String, String> store = new HashMap<>();
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                    store.put(m.getKey(), m.getValue());
+                    System.out.println("receive message! "+m.toString());
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new StoreMessage(req.getKey(), store.get(req.getKey())), self())
+                ).build();
+    }
 }
