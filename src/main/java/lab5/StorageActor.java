c68c9f01b839b3af43e659bbfc5cd780a2f8ada1
izotarina <irunchikt@mail.ru>
2021-12-12 11:48:27 +0300

    Scripted auto-commit on change (2021-12-12 11:48:27) by gitwatch.sh

--- src/main/java/lab5/StorageActor.java
+++ src/main/java/lab5/StorageActor.java
@@ -1,12 +1,14 @@
 package lab5;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class StorageActor extends AbstractActor {
     private Map<String, String> store = new HashMap<>();
+    
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
