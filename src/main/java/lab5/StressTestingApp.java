bdb48cc0bd5df25aae7bede3aab91390c6f4a52c
izotarina <irunchikt@mail.ru>
2021-12-12 11:37:56 +0300

    Scripted auto-commit on change (2021-12-12 11:37:56) by gitwatch.sh

--- src/main/java/lab5/StressTestingApp.java
+++ src/main/java/lab5/StressTestingApp.java
@@ -2,6 +2,7 @@ package lab5;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
@@ -10,6 +11,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class StressTestingApp {
     public static void main(String[] args) throws IOException {
