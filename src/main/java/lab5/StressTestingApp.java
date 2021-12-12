a51e077b955d33cd478097efc4ff4b129467e122
izotarina <irunchikt@mail.ru>
2021-12-12 11:37:09 +0300

    Scripted auto-commit on change (2021-12-12 11:37:09) by gitwatch.sh

--- src/main/java/lab5/StressTestingApp.java
+++ src/main/java/lab5/StressTestingApp.java
@@ -1,5 +1,25 @@
 package lab5;
 
 public class StressTestingApp {
-
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
+        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress
+                RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown
+        when done
+    }
 }
