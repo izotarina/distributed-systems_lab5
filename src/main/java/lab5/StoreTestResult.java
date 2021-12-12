5613a0bc2ed353229cb9423040d12cf0e53f6527
izotarina <irunchikt@mail.ru>
2021-12-12 11:54:38 +0300

    Scripted auto-commit on change (2021-12-12 11:54:38) by gitwatch.sh

--- src/main/java/lab5/StoreTestResult.java
+++ src/main/java/lab5/StoreTestResult.java
@@ -2,5 +2,11 @@ package lab5;
 
 public class StoreTestResult {
     private final String url;
-    private final int 
+    private final int requestCount;
+    private final String result;
+
+    public StoreTestResult(String url, int requestCount, String result) {
+        this.url = url;
+        this.requestCount
+    }
 }
